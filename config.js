572a92633aac0704321df3905ff06bbf819eb574
bricks-repo <myprofitrepo+bricks@gmail.com>
2026-02-28 00:00:24 +0200

    update witn new config

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.js - автономний для кожного репозиторію
-const CONFIG = {
-    version: "1.0.2",
-    updated: "2026-02-27",
-    suggestion_url: "https://docs.google.com/forms/d/...",
-    support_url: "https://www.buymeacoffee.com/...",
-    hub_url: "https://bricks-repo.github.io/"
-};
