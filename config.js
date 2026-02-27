bfa758b8336be29b0761dcb3539855cfd6a59150
bricks-repo <myprofitrepo+bricks@gmail.com>
2026-02-27 16:33:57 +0200

    update with config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+    suggestion_url: "https://docs.google.com/forms/d/e/ВАШ_ID/viewform",
+    support_url: "https://www.buymeacoffee.com/bricks-repo",
+    hub_url: "https://bricks-repo.github.io/"
+};
